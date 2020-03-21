4ecdf0de87f84de63fde2523ecd156c54802c938
DGreegman <36539509+DGreegman@users.noreply.github.com>
2020-03-21 11:03:31 -0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,207 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+<head>
+    <meta charset="utf-8">
+    <title></title>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/style.css?v=4">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+</head>
+    <body>
+
+            <header style="background-color: green; border-radius: 0 0 30px;" class="container-fluid">
+                <nav class="navbar navbar-expand-sm" style="background-color: green;">
+                     <a class="navbar-brand text-white" href="#">EBA</a>
+                     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+            <div class="collapse navbar-collapse" id="collapsibleNavbar">
+                <ul class="navbar-nav ml-auto ">
+                    <li class="nav-item">
+                      <a class="nav-link text-white pr-5" href="#">Home</a>
+                    </li>
+                    <li class="nav-item">
+                      <a class="nav-link text-white pr-5" href="#">Ministries</a>
+                    </li>
+                    <li class="nav-item">
+                      <a class="nav-link text-white pr-5" href="#">Branches</a>
+                    </li>
+                    <li class="nav-item">
+                      <a class="nav-link text-white pr-5" href="#">Programs</a>
+                    </li>
+                    <li class="nav-item">
+                      <a class="nav-link text-white pr-5" href="#">Structure</a>
+                    </li>
+                    <li class="nav-item">
+                      <a class="nav-link text-white pr-5" href="#">Projects</a>
+                    </li>
+                </ul>
+             </div>
+                </nav>
+                <div class="text-center">
+                    <p class="text-center text-uppercase text-white" style="font-family: 'Times New Roman', Times, serif; font-size: 30px;">eha-amufu belivers association</p>
+                    <p class=" text-white">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation </p>
+
+                </div>
+            </header>
+
+            <!--carousel for programs of the year-->
+            <div class="container">
+                <div id="demo" class="carousel slide" data-ride="carousel">
+
+                  <!-- Indicators -->
+                  <ul class="carousel-indicators">
+                    <li data-target="#demo" data-slide-to="0" class="active"></li>
+                    <li data-target="#demo" data-slide-to="1"></li>
+                    <li data-target="#demo" data-slide-to="2"></li>
+                  </ul>
+
+                  <!-- The slideshow -->
+                  <div class="carousel-inner">
+                    <div class="carousel-item active">
+                      <img src="img/carousel 1.jpg" alt="Los Angeles" width="1100" height="500">
+                      <div class="carousel-caption">
+                        <h3>EBA National Convention</h3>
+                        <p>Come and Experience Christ</p>
+                      </div>
+                    </div>
+                    <div class="carousel-item">
+                      <img src="img/carousel 2.jpg" alt="Chicago" width="1100" height="500">
+                    </div>
+                    <div class="carousel-item">
+                      <img src="img/carousel 3.jpg" alt="New York" width="1100" height="500">
+                    </div>
+                  </div>
+
+                  <!-- Left and right controls -->
+                  <a class="carousel-control-prev" href="#demo" data-slide="prev">
+                    <span class="carousel-control-prev-icon"></span>
+                  </a>
+                  <a class="carousel-control-next" href="#demo" data-slide="next">
+                    <span class="carousel-control-next-icon"></span>
+                  </a>
+              </div>
+
+        </div>
+
+<div class="container">
+    <div class="row">
+        <div class="col">
+            <span class="text-uppercase">leaders from iception</span>
+            <div class="flex-container">
+                <div>image 1</div>
+                <div>image 2</div>
+                <div>image 3</div>
+                <div>image 4</div>
+                <div>image 5</div>
+            </div>
+            <span class="text-uppercase">National Excoss</span>
+            <div class="flex-container">
+                <div>image 1</div>
+                <div>image 2</div>
+                <div>image 3</div>
+                <div>image 4</div>
+                <div>image 5</div>
+            </div>
+            <span class="text-uppercase">National Council and Patron</span>
+                <div class="flex-container">
+                    <div>image 1</div>
+                    <div>image 2</div>
+                    <div>image 3</div>
+                    <div>image 4</div>
+                    <div>image 5</div>
+                </div>
+        </div>
+
+    </div>
+</div>
+
+<div class="container">
+    <div class="row">
+        <div class="col-md-3">
+
+            <div class="card">
+              <!-- <div class="card-header">Header</div> -->
+              <div class="card-body">Content</div>
+              <div class="card-footer">
+                  <p class="text-white text-uppercase text-center">info about eha-land</p>
+                  <p class="text-white">Lorem ipsum dolor sit amet, consectetur adipisicing elit</p>
+                  <a href="#" class="card-link text-center text-white d-block" style="border: 1px solid white"> more</a>
+              </div>
+            </div>
+        </div>
+        <div class="col-md-3">
+
+            <div class="card">
+              <!-- <div class="card-header">Header</div> -->
+              <div class="card-body">Content</div>
+              <div class="card-footer">
+                  <p class="text-white text-uppercase text-center">history of EBA</p>
+                  <p class="text-white">Lorem ipsum dolor sit amet, consectetur adipisicing elit</p>
+                  <a href="#" class="card-link text-center text-white d-block" style="border: 1px solid white"> more</a>
+              </div>
+            </div>
+        </div>
+        <div class="col-md-3">
+
+            <div class="card">
+              <!-- <div class="card-header">Header</div> -->
+              <div class="card-body">Content</div>
+              <div class="card-footer">
+                  <p class="text-white text-uppercase text-center">imapact of eba in eha</p>
+                  <p class="text-white">Lorem ipsum dolor sit amet, consectetur adipisicing elit</p>
+                  <a href="#" class="card-link text-center text-white d-block" style="border: 1px solid white"> more</a>
+              </div>
+            </div>
+        </div>
+        <div class="col-md-3">
+
+            <div class="card">
+              <!-- <div class="card-header">Header</div> -->
+              <div class="card-body">Content</div>
+              <div class="card-footer">
+                  <p class="text-white text-uppercase text-center">aims and obj of eba</p>
+                  <p class="text-white">Lorem ipsum dolor sit amet, consectetur adipisicing elit</p>
+                  <a href="#" class="card-link text-center text-white d-block" style="border: 1px solid white"> more</a>
+              </div>
+            </div>
+        </div>
+    </div>
+</div>
+<footer class="mt-3">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-3">
+                <p class="text-uppercase text-white mt-3">contact us</p>
+                <span class="text-white d-block" >Lorem ipsum</span>
+                <span class="text-white d-block">Lorem ipsum</span>
+                <span class="text-white d-block">Lorem ipsum</span>
+
+            </div>
+            <div class="col-md-3">
+                <p class="text-uppercase text-white mt-3">contact us</p>
+                <span class="text-white d-block">Lorem ipsum</span>
+                <span class="text-white d-block">Lorem ipsum</span>
+                <span class="text-white d-block">Lorem ipsum</span>
+            </div>
+            <div class="col-md-3">
+                <p class="text-uppercase text-white mt-3">contact us</p>
+                <span class="text-white d-block">Lorem ipsum</span>
+                <span class="text-white d-block">Lorem ipsum</span>
+                <span class="text-white d-block">Lorem ipsum</span>
+            </div>
+            <div class="col-md-3">
+                <p class="text-uppercase text-white mt-3">contact us</p>
+                <span class="text-white d-block">Lorem ipsum</span>
+                <span class="text-white d-block">Lorem ipsum</span>
+                <span class="text-white d-block">Lorem ipsum</span>
+            </div>
+        </div>
+    </div>
+</footer>
+
+    </body>
+<script type="text/javascript" src="js/jquery-3.4.1.min.js"></script>
+<script type="text/javascript" src="js/bootstrap.min.js"></script>
+</html>
